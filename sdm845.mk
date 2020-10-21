1f14912b1b0a4af6b7e95044d55b1f3928fc91e9
Lup Gabriel <gwolf2u.website@gmail.com>
2020-10-21 19:42:26 +0300

    [TMP] sdm845-common: remove VR

--- sdm845.mk
+++ sdm845.mk
@@ -319,7 +319,7 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VR
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
     android.hardware.vr@1.0-service \
     vr.sdm845
